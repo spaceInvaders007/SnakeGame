17d096f49d4497455a7bfc70ff39cb2e3f3c2cac
Ignacio <ignacio.senese@abx.com>
2023-10-05 22:26:46 +0900

    Point moves continuously until hitting the boundary or changed direction

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,51 +1,81 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState } from "react";
 
 export const Canvas: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const [pos, setPos] = useState({ x: 200, y: 200 });
+  const movementInterval = useRef<number | null>(null);
 
-  useEffect(() => {
-    const handleKeydown = (e: KeyboardEvent) => {
-        let newX = pos.x;
-        let newY = pos.y;
-        const moveAmount = 10;  
-        const radius = 10;     
-      
+  const handleKeydown = (e: KeyboardEvent) => {
+    if (movementInterval.current) {
+      clearInterval(movementInterval.current);
+    }
+
+    const moveAmount = 10;
+    const radius = 10;
+
+    movementInterval.current = window.setInterval(() => {
+      setPos((prevPos) => {
+        let newX = prevPos.x;
+        let newY = prevPos.y;
+
         switch (e.key) {
-          case 'ArrowUp':
+          case "ArrowUp":
             newY = Math.max(newY - moveAmount, radius);
             break;
-          case 'ArrowDown':
-            newY = Math.min(newY + moveAmount, 400 - radius);  
+          case "ArrowDown":
+            newY = Math.min(newY + moveAmount, 400 - radius);
             break;
-          case 'ArrowLeft':
-            newX = Math.max(newX - moveAmount, radius); 
+          case "ArrowLeft":
+            newX = Math.max(newX - moveAmount, radius);
             break;
-          case 'ArrowRight':
-            newX = Math.min(newX + moveAmount, 400 - radius);  
+          case "ArrowRight":
+            newX = Math.min(newX + moveAmount, 400 - radius);
             break;
         }
-      
-        setPos({ x: newX, y: newY });
-      };
-      
 
-    window.addEventListener('keydown', handleKeydown);
-    return () => window.removeEventListener('keydown', handleKeydown);
-  }, [pos]);
+        if (
+          newY === radius ||
+          newY === 400 - radius ||
+          newX === radius ||
+          newX === 400 - radius
+        ) {
+          if (movementInterval.current !== null) {
+            clearInterval(movementInterval.current);
+          }
+        }
+
+        return { x: newX, y: newY };
+      });
+    }, 100);
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeydown);
+    return () => {
+      window.removeEventListener("keydown", handleKeydown);
+    };
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      if (movementInterval.current !== null) {
+        clearInterval(movementInterval.current);
+      }
+    };
+  }, []);
 
   useEffect(() => {
     if (canvasRef.current) {
-      const ctx = canvasRef.current.getContext('2d');
+      const ctx = canvasRef.current.getContext("2d");
       if (ctx) {
-        ctx.clearRect(0, 0, 400, 400);  
+        ctx.clearRect(0, 0, 400, 400);
         ctx.beginPath();
-        ctx.arc(pos.x, pos.y, 10, 0, Math.PI * 2); 
-        ctx.fillStyle = 'blue';
+        ctx.arc(pos.x, pos.y, 10, 0, Math.PI * 2);
+        ctx.fillStyle = "blue";
         ctx.fill();
       }
     }
   }, [pos]);
 
   return <canvas ref={canvasRef} width={400} height={400}></canvas>;
-}
+};
